57a41c0f410f345be11adc245216d3e60f93784a
unknown <lucasfmancan@hotmail.com>
2020-08-27 21:20:09 -0300

    Heroku teste

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,6 +2,4 @@ import { userService } from '../services/';
 import { UserController } from './UserController';
 
 const userController = new UserController(userService);
-
-
 export { userController };
