9663650120a9e6ce795f9a19759ba623b600ad8e
unknown <lucasfmancan@hotmail.com>
2020-08-28 09:15:49 -0300

    Ajustando documentação

--- src/server.ts
+++ src/server.ts
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 
 app.use(function(req, res, next) {
   res.status(404);
-  res.send('Ooops.. this is not a valid route, check api docs: /api-docs');
+  res.json({message:'Ooops.. this is not a valid route, check api docs: /api-docs'});
 });
 
 const server = app.listen(port, () =>
